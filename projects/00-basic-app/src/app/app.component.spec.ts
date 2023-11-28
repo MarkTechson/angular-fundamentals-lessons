f618bdff06bac8a7cd84b853945f815dd712f70e
marktechson <2554588+MarkTechson@users.noreply.github.com>
2023-11-27 21:42:03 -0600

    adding code for the second example project

--- projects/00-basic-app/src/app/app.component.spec.ts
+++ projects/00-basic-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '01-hello-angular' title`, () => {
+  it(`should have the '00-basic-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('01-hello-angular');
+    expect(app.title).toEqual('00-basic-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 01-hello-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 00-basic-app');
   });
 });
