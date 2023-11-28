f68997006a6dc3d5d18c65b7daa2f23daf822876
marktechson <2554588+MarkTechson@users.noreply.github.com>
2023-11-28 15:23:22 -0600

    Adds 02-displaying-dynamic-data lesson.

---
+++ projects/02-displaying-dynamic-data/README.md
@@ -0,0 +1,17 @@
+# 02-displaying-dynamic-data
+
+In this example, you'll display dynamic data to the user with the power of [interpolation in Angular](https://angular.dev/guide/templates/interpolation).
+
+## Instructions
+
+1. In `app.component.ts`, update the `template` property to use interoplation to display real values from the `item` property defined in the component class.
+
+2. Save your changes.
+
+3. Stop any previous applications that may be running. Use the following command to display your application in the browser.
+
+   ```bash
+   ng serve 02-displaying-dynamic-data
+   ```
+
+4. Confirm that the `item` data is propertly being displayed in the browser window.
---
+++ projects/02-displaying-dynamic-data/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <article class="offer">
+      <h1>
+        <span>Bonus Offer</span>
+        <span>$<!-- ITEM PRICE --></span>
+      </h1>
+      <img src="/assets/noun-product-6277512.png" width="400" />
+      <p><!-- ITEM NAME--></p>
+      <p><!-- ITEM DESCRIPTION --></p>
+      <button>Order Now</button>
+    </article>
+  `,
+  styles: `
+    .offer {
+      font-family: Verdana;
+      border: solid 1px gray;
+      width: 400px;
+      padding: 20px;
+      border-radius: 3px;
+      color: white;
+      background: #7f6b41;
+    }
+    h1 {
+      display: flex;
+      justify-content: space-between;
+    }
+    button {
+      display: block;
+      width: 100%;
+      padding: 10px;
+      border: solid 1px white;
+      border-radius: 3px;
+    }
+  `,
+})
+export class AppComponent {
+  item = {
+    name: 'Treasure Trove Trunk',
+    price: 30,
+    description:
+      'Unveil a treasure trove of surprises in this delightful mystery box.',
+  };
+}
