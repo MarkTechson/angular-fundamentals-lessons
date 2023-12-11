61e60266b4c66efa5c58170465226ecae72687fa
marktechson <2554588+MarkTechson@users.noreply.github.com>
2023-12-11 12:11:21 -0600

    add signals solutions

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -8,12 +8,55 @@ import { Todo } from './todo';
   imports: [CommonModule],
   template: `
     <h1>Building a TODO List</h1>
-
-    
+    @for(todo of todos(); track todo.id) {
+    <label
+      [ngStyle]="{
+        'text-decoration': todo.completed ? 'line-through' : 'none'
+      }"
+    >
+      <input
+        type="checkbox"
+        [checked]="todo.completed"
+        (change)="updateTodo(todo)"
+      />
+      {{ todo.title }}
+    </label>
+    }
   `,
-  styles: [],
+  styles: [
+    `
+      label {
+        display: block;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  
-  updateTodo(todo: Todo) {}
+  todos = signal<Todo[]>([
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn TypeScript',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn RxJS',
+      completed: false,
+    },
+  ]);
+
+  updateTodo(todo: Todo) {
+    this.todos.update((todoList) =>
+      todoList.map((todoEntry) => {
+        if (todo.id === todoEntry.id)
+          todoEntry.completed = !todoEntry.completed;
+        return todoEntry;
+      })
+    );
+  }
 }
